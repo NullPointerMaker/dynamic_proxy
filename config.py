8ecef9ea68e14120e01a839d9ff1fcf6669325b6
NullPointerMaker <76866070+NullPointerMaker@users.noreply.github.com>
2021-06-10 00:57:35 +0800

    Use list instead od tuple

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 # valid option: http, https, socks4, socks5
 # type can not be null
 # http not include https
-proxy_type = ('https', 'socks4', 'socks5')
+proxy_type = ['https', 'socks4', 'socks5']
 # valid option: transparent, anonymous, elite
 # anonymity can not set
-proxy_anonymity = ('anonymous', 'elite')
+proxy_anonymity = ['anonymous', 'elite']
 # country not set = all
-proxy_country = ()
-proxy_country_exclude = ('CN', 'MO')
+proxy_country = []
+proxy_country_exclude = ['CN', 'MO']
 
 local_host = '127.0.0.1'
-local_port = 1080
+local_port = 1081
 
 rotate_interval = 600
