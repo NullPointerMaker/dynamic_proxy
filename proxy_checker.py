4c3f1fd25f0bd186382d3e354378673c0f481368
NullPointerMaker <76866070+NullPointerMaker@users.noreply.github.com>
2021-06-10 18:20:14 +0800

    Fix checks

--- proxy_checker.py
+++ proxy_checker.py
@@ -1,14 +1,20 @@
 import logging
+from json import JSONDecodeError
 from threading import Timer
 from typing import Optional
 
-import requests
+import peewee
 import socks
-from peewee import fn
-from requests import RequestException
+from requests import Session, RequestException
+from requests.adapters import HTTPAdapter
 
 import config
 from proxy_filter import Proxy, is_valid, delete_proxy
+
+timeout = 60
+session = Session()
+session.mount('http://', HTTPAdapter(max_retries=5))
+session.mount('https://', HTTPAdapter(max_retries=5))
 
 checked_proxy = dict()
 
@@ -23,7 +29,7 @@ def set_checked_proxy(**kwargs):
 
 
 def get_local_ip() -> str:
-    r = requests.get('https://httpbin.org/ip')
+    r = session.get('https://httpbin.org/ip')
     return r.json()['origin']
 
 
@@ -32,7 +38,7 @@ local_ip = get_local_ip()
 
 def check_access(proxies: dict) -> bool:
     try:
-        r = requests.options('http://twitter.com', proxies=proxies)
+        r = session.head('http://twitter.com', proxies=proxies)
         return r.status_code > 0
     except RequestException:  # bad proxy
         return False
@@ -42,8 +48,8 @@ def check_ssl(proxies: dict) -> bool:
     if 'http' in config.proxy_type:  # accept plain
         return True
     try:
-        r = requests.options('https://httpbin.org/status/233', proxies=proxies)
-        return r.status_code == 233
+        r = session.head('https://httpbin.org', proxies=proxies)
+        return r.status_code > 0
     except RequestException:  # bad proxy
         return False
 
@@ -52,10 +58,10 @@ def check_anonymity(proxies: dict) -> bool:
     if 'transparent' in config.proxy_anonymity:  # accept transparent
         return True
     try:
-        r = requests.get('http://httpbin.org/ip', proxies=proxies)
+        r = session.get('http://httpbin.org/ip', proxies=proxies, timeout=timeout)
         if 'anonymous' in config.proxy_anonymity:  # accept anonymous
             return local_ip not in r.text
-        r = requests.get('http://httpbin.org/headers', proxies=proxies)
+        r = session.get('http://httpbin.org/headers', proxies=proxies, timeout=timeout)
         if 'elite' in config.proxy_anonymity:  # accept elite
             headers = r.json()['headers']
             return 'Via' not in headers and 'X-Forwarded-For' not in headers  # todo valid?
@@ -66,10 +72,15 @@ def check_anonymity(proxies: dict) -> bool:
 def check_country(proxies: dict) -> bool:
     if not config.proxy_country and not config.proxy_country_exclude:  # accept all countries
         return True
+    r = None
     try:
-        r = requests.get('https://echo.copythat.workers.dev', proxies=proxies)
+        r = session.get('https://echo.copythat.workers.dev', proxies=proxies, timeout=timeout)
         country = r.json()['cf']['country']
-    except RequestException:  # bad proxy
+    except RequestException as e:  # bad proxy
+        logging.debug(e)
+        return False
+    except JSONDecodeError:
+        logging.debug(r.text)
         return False
     if config.proxy_country and country not in config.proxy_country:
         return False
@@ -80,7 +91,7 @@ def check_country(proxies: dict) -> bool:
 
 def access_weixin(proxies: dict) -> bool:
     try:
-        r = requests.get('http://mp.weixin.qq.com', proxies=proxies)
+        r = session.get('https://mp.weixin.qq.com', proxies=proxies, timeout=timeout)
         if r.status_code != 200:  # api offline
             return False
         return 'Chrome' in r.text
@@ -89,7 +100,7 @@ def access_weixin(proxies: dict) -> bool:
 
 
 def check_proxy() -> Optional[Proxy]:
-    random_proxy: Proxy = Proxy.select().order_by(fn.Random()).get()
+    random_proxy: Proxy = Proxy.select().order_by(peewee.fn.Random()).get()
     logging.info('%s: %s' % (check_proxy.__name__, random_proxy.address))
     if not is_valid(random_proxy):
         logging.warning('%s: %s not valid' % (check_proxy.__name__, random_proxy.address))
